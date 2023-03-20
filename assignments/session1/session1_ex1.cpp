75174751cb5c4509fa211a82527a14c01c9d0c44
Charles <charles.do@singalarity.com>
2023-03-20 22:05:06 +0700

    [Updated] added some file

---
+++ assignments/session1/session1_ex1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n = 0, result = 0;
+  cin >> n;
+  result = (n + 1) / 2;
+  cout << "Trung binh cong 1 den " << n << " la : " << result << endl;
+  return 0;
+}
