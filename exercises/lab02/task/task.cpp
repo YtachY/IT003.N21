59f9d45453276f849179341491dc9ce33426ea7e
DoDoChi <dodongchien1232@gmail.com>
2023-04-07 23:09:35 +0700

    feat(task): :sparkles: ACCEPTED SUBMISSION

--- exercises/lab02/task/task.cpp
+++ exercises/lab02/task/task.cpp
@@ -1,77 +1,66 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 const int MAX = 1e9;
 
+pair<int, int> transferIndex(int x) {
+  pair<int, int> result = {0, 0};
+  if (x % 2 == 0) {
+    result.first = x / 2;
+    result.second = 2;
+  } else {
+    result.first = x / 2 + 1;
+    result.second = 1;
+  }
+  return result;
+}
+
+int findRealIndex(int p, int q) {
+  int result;
+  if (q == 2) {
+    result = p * 2;
+  } else {
+    result = p * 2 - 1;
+  }
+  // cout << ">>> Coordinate: " << p << ' ' << q << endl;
+  // cout << ">>> RealIndex: " << result << endl;
+  return result;
+}
 void readInput(int &a, int &b, int &c, int &d) {
   ios::sync_with_stdio(0);
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
   cin >> a >> b >> c >> d;
-  // for (int i = 0; i < a; ++i) {
-  //   vector <int> temp;
-  //   for (int j = 0; j < b; ++j) {
-  //     int t = 0;
-  //     cin >> t;
-  //     temp.push_back(t);
-  //   }
-  //   v.push_back(temp);
-  // }
 }
 
 int main() {
-  // vector <vector <int>> a;
   int n, k, p, q;
   int u, v;
-  int realIndex = 0;
+  int realIndexAlice = 0;
+  int realIndexBob = 0;
   bool isValid = true;
+  pair<int, int> result = {0, 0};
   readInput(n, k, p, q);
 
-  if (q % 2 == 0) {
-    realIndex = p * 2;
-  }
-  else {
-    realIndex = p * 2 - 1;
-  }
-  int tempFront = realIndex - k;
-  int tempBack = realIndex + k;
-  if (tempFront >= 1) {
-    u = tempFront / 2 + 1;
-    if (tempFront % 2 == 0) {
-      v = 2;
+  realIndexAlice = findRealIndex(p, q);
+  realIndexBob = realIndexAlice - k;
+  if (realIndexBob >= 1) {
+    result = transferIndex(realIndexBob);
+  } else {
+    realIndexBob = realIndexAlice + k;
+    if (realIndexBob <= n) {
+      result = transferIndex(realIndexBob);
+    } else {
+      isValid = false;
     }
-    else {
-      v = 1;
-    }
-  }
-  else if (tempBack <= n) {
-    u = tempBack / 2;
-    if (tempBack % 2 == 0) {
-      v = 2;
-    }
-    else {
-      v = 1;
-    }
-  }
-  else {
-    isValid = false;
   }
   if (isValid) {
-    cout << u << ' ' << v;
-  }
-  else {
+    cout << result.first << " " << result.second;
+  } else {
     cout << -1;
   }
-  // for (int i = 0; i < n; ++i) {
-  //   for (int j = 0; j < m; ++j) {
-  //     cout << a[i][j] << ' ';
-  //   }
-  //   cout << endl;
-  // }
-
   return 0;
 }
