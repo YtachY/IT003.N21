720b36e7ec0cd06a27f7d00efae56f310b070e99
DoDoChi <dodongchien1232@gmail.com>
2023-04-08 17:59:46 +0700

    perf(kiemKe): :zap: 3 TLE testcase remaining

---
+++ exercises/lab02/kiemKe/deBai.md
@@ -0,0 +1,128 @@
+**Kiểm kê** 
+============
+
+   Cửa hàng X có nhiều loại hàng, mỗi loại hàng đều có mã khác nhau. Tuy nhiên, trong lúc bán hàng, nhân viên quên không nhập số lượng mỗi loại mà chỉ nhập mã các loại hàng trên từng dòng. Bạn hãy đếm xem có bao nhiêu loại hàng khác nhau?  
+
+**Ví dụ:** Có một số loại hàng có mã lần lượt là  \[123, 45, 8, 19, 1, 2333\]. 
+
+Bạn nhân viên sẽ nhập theo từng dòng \[123, 45, 45, 8, 1, 123, 123, 8, 8\]. 
+
+Số loại hàng khác nhau trong dãy được nhập sẽ là 4.  
+
+  
+ 
+
+**Input:** 
+
+*   Dòng đầu tiên là  số nguyên NNN (1≤N≤5×104)(1≤N≤5×104)(1 \\leq N \\leq 5\\times 10^{4}) là số lượng mã loại hàng được nhập.
+    
+*   NNN dòng tiếp theo, mỗi dòng là một mã của loại hàng được nhập, độ dài của mã có thể lên tới 100.
+    
+
+**Output:**
+
+*   Một dòng chứa số nguyên là số các loại hàng khác nhau. 
+    
+
+**Input**
+
+**Output**
+
+10
+
+123
+
+123444
+
+123444
+
+12
+
+1
+
+8
+
+1
+
+455
+
+455
+
+9
+
+7
+
+  
+  
+  
+  
+  
+ 
+
+\[IT003.N24.2\] Assignment 2  
+
+8 problems with a total score of 800
+
+#
+
+Problem
+
+Score
+
+1
+
+[Task](https://khmt.uit.edu.vn/wecode/it003/assignment/138/879)
+
+100
+
+2
+
+[Point3D](https://khmt.uit.edu.vn/wecode/it003/assignment/138/878)
+
+100
+
+3
+
+[khangtd.DetectVirus](https://khmt.uit.edu.vn/wecode/it003/assignment/138/766)
+
+100
+
+4
+
+[an.512.Trộn 2 mảng](https://khmt.uit.edu.vn/wecode/it003/assignment/138/512)
+
+100
+
+5
+
+[Find MEX](https://khmt.uit.edu.vn/wecode/it003/assignment/138/880)
+
+100
+
+6
+
+[Point2D (template)](https://khmt.uit.edu.vn/wecode/it003/assignment/138/881)
+
+100
+
+7
+
+[Kiểm Kê](https://khmt.uit.edu.vn/wecode/it003/assignment/138/882)
+
+100
+
+8
+
+[VQ44\_FLOWERS(template)](https://khmt.uit.edu.vn/wecode/it003/assignment/138/883)
+
+100
+
+Submit
+
+  
+
+upload source code 
+
+C++ (1s, 50MB ) Select language
+
+[Code editor](https://khmt.uit.edu.vn/wecode/it003/submissions/create/assignment/138/problem/882)
--- exercises/lab02/kiemKe/kiemKe.cpp
+++ exercises/lab02/kiemKe/kiemKe.cpp
@@ -2,85 +2,55 @@
 #include <string>
 #include <vector>
 
-#define ll long long
 using namespace std;
 
 const int MAX = 5e4;
 
-void heapify(vector<ll>& v, int n, int i) {
-  int largest = i;        // Initialize largest as root
-  int left = 2 * i + 1;   // left child index
-  int right = 2 * i + 2;  // right child index
-
-  // If left child is larger than root
-  if (left < n && v[left] > v[largest])
-    largest = left;
-
-  // If right child is larger than largest so far
-  if (right < n && v[right] > v[largest])
-    largest = right;
-
-  // If largest is not root
-  if (largest != i) {
-    swap(v[i], v[largest]);
-
-    // Recursively heapify the affected sub-tree
-    heapify(v, n, largest);
-  }
-}
-
-void heapSort(vector<ll>& v) {
-  int n = v.size();
-
-  // Build max heap
-  for (int i = n / 2 - 1; i >= 0; i--)
-    heapify(v, n, i);
-
-  // Extract elements from heap one by one
-  for (int i = n - 1; i >= 0; i--) {
-    swap(v[0], v[i]);
-
-    // Call max heapify on the reduced heap
-    heapify(v, i, 0);
-  }
-}
-
-void readInput(vector<ll>& v, int n) {
+void readInput(vector<string>& v, int& n) {
   ios::sync_with_stdio(0);
-  // freopen("input.txt", "r", stdin);
+  freopen("input.txt", "r", stdin);
 
   cin >> n;
 
   for (int i = 0; i < n; ++i) {
-    ll temp = 0;
+    string temp;
     cin >> temp;
     v.push_back(temp);
   }
 }
-void printArray(vector<ll> v) {
+void printArray(vector<string> v) {
   for (int i = 0; i < v.size(); ++i) {
     cout << v[i] << ' ';
   }
 }
 
+int countDistinct(vector<string> arr, int n) {
+  int res = 1;
+
+  // Pick all elements one by one
+  for (int i = 1; i < n; i++) {
+    int j = 0;
+    for (j = 0; j < i; j++)
+      if (arr[i] == arr[j])
+        break;
+
+    // If not printed earlier, then print it
+    if (i == j)
+      res++;
+  }
+  return res;
+}
+
 int main() {
   int n;
-  int result = 0;
-  vector<ll> myArray;
+  int result = 1;
+  bool flag = false;
+  vector<string> myArray;
   readInput(myArray, n);
 
-  heapSort(myArray);
-
-  for (int i = 0; i < myArray.size(); ++i) {
-    // if (myArray[i] > myArray[i - 1]) {
-    //   ++result;
-    // }
-    while (i < myArray.size() && myArray[i] == myArray[i + 1])
-      i++;
-
-    result++;
-  }
-  cout << result;
+  // heapSort(myArray);
+  result = countDistinct(myArray, n);
+  cout << result << endl;
   // printArray(myArray);
   return 0;
 }
