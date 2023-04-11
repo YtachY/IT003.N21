76628a83e5e9d92c3d989d35c9e3352ae8429850
DoDoChi <dodongchien1232@gmail.com>
2023-04-11 21:42:41 +0700

    perf(kiemke): :zap: ACCEPTED SUBMISSION

--- exercises/lab02/kiemKe/kiemKe.cpp
+++ exercises/lab02/kiemKe/kiemKe.cpp
@@ -1,56 +1,74 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
 using namespace std;
 
-const int MAX = 5e4;
+void heapify(double arr[], int n, int i) {
+  int largest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
 
-void readInput(vector<string>& v, int& n) {
-  ios::sync_with_stdio(0);
-  freopen("input.txt", "r", stdin);
+  if (l < n && arr[l] > arr[largest])
+    largest = l;
 
-  cin >> n;
+  if (r < n && arr[r] > arr[largest])
+    largest = r;
 
-  for (int i = 0; i < n; ++i) {
-    string temp;
-    cin >> temp;
-    v.push_back(temp);
-  }
-}
-void printArray(vector<string> v) {
-  for (int i = 0; i < v.size(); ++i) {
-    cout << v[i] << ' ';
+  if (largest != i) {
+    swap(arr[i], arr[largest]);
+    heapify(arr, n, largest);
   }
 }
 
-int countDistinct(vector<string> arr, int n) {
-  int res = 1;
+void heapSort(double arr[], int n) {
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapify(arr, n, i);
 
-  // Pick all elements one by one
-  for (int i = 1; i < n; i++) {
-    int j = 0;
-    for (j = 0; j < i; j++)
-      if (arr[i] == arr[j])
-        break;
-
-    // If not printed earlier, then print it
-    if (i == j)
-      res++;
+  for (int i = n - 1; i >= 0; i--) {
+    swap(arr[0], arr[i]);
+    heapify(arr, i, 0);
   }
-  return res;
+}
+
+void quickSort(double* a, int b, int e) {
+  if (b >= e) return;
+  double Pivot = a[(b + e) / 2];
+  int i = b, j = e;
+  while (i < j) {
+    while (a[i] < Pivot) i++;
+    while (a[j] > Pivot) j--;
+    if (i <= j) {
+      swap(a[i], a[j]);
+      i++;
+      j--;
+    }
+  }
+  quickSort(a, b, j);
+  quickSort(a, i, e);
 }
 
 int main() {
+  ios::sync_with_stdio(0);
+  freopen("input.txt", "r", stdin);
   int n;
-  int result = 1;
-  bool flag = false;
-  vector<string> myArray;
-  readInput(myArray, n);
-
-  // heapSort(myArray);
-  result = countDistinct(myArray, n);
-  cout << result << endl;
-  // printArray(myArray);
+  cin >> n;
+  if (n == 0) {
+    cout << 0;
+    return 0;
+  }
+  double* A = new double[n];
+  for (int i = 0; i < n; i++) {
+    cin >> A[i];
+  }
+  // quickSort(A, 0, n - 1);
+  heapSort(A, n);
+  double X = A[0];
+  int dem = 1;
+  for (int i = 0; i < n; i++)
+    if (A[i] != X) {
+      dem++;
+      X = A[i];
+    }
+  delete (A);
+  cout << dem;
   return 0;
 }
