1a9d40462737add6fef1741bb028c4771dd683e4
DoDoChi <dodongchien1232@gmail.com>
2023-05-22 16:04:02 +0700

    feat(reverseLinkedList): :sparkles: ACCEPTED SUBMISSION

---
+++ exercises/lab03/reverseLinkedList/template_reverseLinkedList.cpp
@@ -0,0 +1,72 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+std
+###End banned keyword*/
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+    
+};
+
+
+
+
+// Complete the insertSortedLinkedList function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+//###INSERT CODE HERE -
+
+int main()
+{
+    SinglyLinkedList* llist = new SinglyLinkedList();
+    int llist_count;
+
+
+    cin >> llist_count;
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+
+        insert_node(llist,llist_item);
+    }
+    
+    
+    reverseLinkedList(llist);
+    printLinkedList(llist);
+
+    return 0;
+}
